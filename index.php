6df4b3cc261d6894df24e1572ff03750655527de
DaveKirby111 <dkirby111@aol.com>
2022-12-09 15:38:38 -0600

    added starter files

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <script src="js/bootstrap.bundle.js"></script>
+    <title>Mass Effect Fansite</title>
+    <link rel="stylesheet" href="styles/screen.css">
+</head>
+<body>
+
+<div class="content">
+
+<header>
+    <h1>mass effect fansite</h1>
+</header>
+
+<nav>
+    <ul>
+        <li><a href="">link</a></li>
+        <li><a href="">link</a></li>
+        <li><a href="">link</a></li>
+    </ul>
+</nav>
+
+<main class="container-fluid">
+
+</main>
+
+<footer>
+    <p>author: david kirby</p>
+</footer>
+
+</div>
+    
+</body>
+</html>
